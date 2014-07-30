fe33f9ad990b05fff2eea9344a9892ba11cee534
Clarkok <mail@clarkok.com>
2014-07-30 20:21:32 +0800

    user model added

---
+++ application/models/user.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class User extends CI_Model {
+    const API = 'http://passport.myqsc.com/api/get_member_by_token?token=';
+
+    function __construct () {
+        parent::__construct();
+    }
+
+    function validate () {
+        $token = get_cookie('token');
+
+        if ($token) {
+            $res = $this->send_get_request(self::API . $token);
+            if (strlen($res['error'])) {
+                return false;
+            }
+            else {
+                $data = json_decode($res['data']);
+                return $data->username;
+            }
+        }
+        else {
+            return false;
+        }
+    }
+
+    function send_get_request ($url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+
+        $result = array(
+            'data' => '',
+            'error' => ''
+        );
+
+        $response = curl_exec($ch);
+
+        $result['error'] = curl_error($ch);
+        $result['data'] = substr($response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
+
+        return $result;
+    }
+}
+
